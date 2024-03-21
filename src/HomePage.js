393965c0120dfbb1d2f8fb643c0134a8706d05b0
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-03-21 13:39:07 +0600

    add bg

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,7 +7,7 @@ export const HomePage =() => {
     return (
         <div>
            {/* Navigation*/}
-            <section className="container mx-auto p-6">
+            <section className="container mx-auto p-6 relative">
                 {/*------------------Flex Container ---------*/}
                 <div className="flex items-center justify-between">
                 {/*------Logo ---*/}
@@ -26,14 +26,19 @@ export const HomePage =() => {
                     hover:bg-brightRedLight cursor-pointer md:block">
                         Get Started
                     </a>
+                {/*-----Hamburger Icon ------*/}
+
                 </div>
+
+
             </section>
 
             {/*------ HERO SECTION -----------*/}
             <section id="hero">
-            {/*---- FLEX CONTAINER ------------*/}
-                <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
-                    {/*----- LEFT ITEM --------*/}
+                {/*---- FLEX CONTAINER ------------*/}
+                <div
+                    className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
+                {/*----- LEFT ITEM --------*/}
                     <div className="flex flex-col mb-32 space-y-12 md:w-1/2 ">
                         {/*---- Hero Text ------*/}
                         <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
